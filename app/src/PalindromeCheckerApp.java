73547263d2ce8b6397a478478f560da7454eeb57
al344 <alanjacobmmiv2@gmail.com>
2026-02-26 10:09:04 +0530

    Added palindrome checker program

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    static void main() {
-        System.out.println("Hello");
-        System.out.println("Welcome to the PalindromeCheckerApp");
-        System.out.println("Version 1.1");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        String cleaned = original.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("\"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("\"" + original + "\" is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
-
